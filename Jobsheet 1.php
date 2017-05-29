126704f6b2a215a0a33914d2fddfa1cb2d09fe2c
Rizka Marta Kartikasari <Rizkamarta05@gmail.com>
2017-05-29 12:25:46 +0700

    Add files via upload

---
+++ Jobsheet 1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>contoh pemakaian PHP</title>
+</head>
+<body>
+<script language="php">
+echo ("Aku sekarang bisa PHP! <br>");
+print ("Mudah dan menyenangkan<br>");
+</script>
+<?php echo("ini juga bisa <br>"); ?>
+
+<script language="php">
+echo ("Tulis pake ini jika html editor kita tidak mengenal PHP");
+/* teks komentar satu batris*/
+
+</script>
+</body>
+</html>
---
+++ Jobsheet 2.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Contoh Pemakaian Teks Komentar </title>
+</head>
+<body>
+<?php
+/* echo "Aku  sekarang bisa PHP! <br>";
+print "Mudah dan menyenangkan<br>";
+?>
+
+<?php echo("Ini juga bisa <br>"); */ ?>
+
+<script language="php">
+echo ("Tulis pake ini jika html editor kita tidak mengenal PHP");
+/* teks komentar satu batris*/
+
+</script>
+</body>
+</html> 
